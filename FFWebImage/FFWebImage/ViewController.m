75b0a26afd596c503cfe52a9f45e1c91327fe4b2
范英强 <smile_faner@163.com>
2017-02-23 19:06:58 +0800

    01-创建项目

---
+++ FFWebImage/FFWebImage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFWebImage
+//
+//  Created by smile on 17/2/23.
+//  Copyright © 2017年 haha. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ FFWebImage/FFWebImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FFWebImage
+//
+//  Created by smile on 17/2/23.
+//  Copyright © 2017年 haha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
